0454213044e3fafeb70f741dcb85289ae9406dfe
Adrien Etienne <a.etienne92@gmail.com>
2021-03-02 09:51:23 +0100

    feat(database): ajout produces saison colonnes

--- src/app/produce/infrastructure/ProduceEntity.ts
+++ src/app/produce/infrastructure/ProduceEntity.ts
@@ -7,4 +7,22 @@ export class ProduceEntity {
 
   @Column({unique: true, length: 50, nullable: false})
   name: string;
+
+  /**
+   * Premier mois de la saison.
+   * Valeur est entre 1 et 12: le premier mois de la saison.
+   * Valeur est autre: pas de saison, la disponibilité est annuelle.
+   * @default 0 Par default la disponibilité est annuelle.
+   */
+  @Column({type: "smallint", default: 0})
+  seasonFirstMonth = 0;
+
+  /**
+   * Le nombre de mois de la saison.
+   * Valeur est entre 1 et 11: le nombre de mois de la saison.
+   * Valeur est autre: pas de saison, la disponibilité est annuelle.
+   * @default 0 Par default la disponibilité est annuelle.
+   */
+  @Column({type: "smallint", default: 0})
+  seasonMonthsCount = 0;
 }
