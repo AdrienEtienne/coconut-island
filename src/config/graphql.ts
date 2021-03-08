1a2554e0d0707fa83b66fbd63647cd74511232e1
Adrien Etienne <a.etienne92@gmail.com>
2021-03-08 13:03:58 +0100

    ci(config): add graphql env variables

--- src/config/graphql.ts
+++ src/config/graphql.ts
@@ -1,4 +1,4 @@
 import {NODE_ENV} from "./config";
 
-export const GRAPHQL_DEBUG = NODE_ENV !== "production";
-export const GRAPHQL_PLAYGROUND = NODE_ENV !== "production";
+export const GRAPHQL_DEBUG = process.env.GRAPHQL_DEBUG ? process.env.GRAPHQL_DEBUG === "true" : NODE_ENV !== "production";
+export const GRAPHQL_PLAYGROUND = process.env.GRAPHQL_PLAYGROUND ? process.env.GRAPHQL_PLAYGROUND === "true" : NODE_ENV !== "production";
